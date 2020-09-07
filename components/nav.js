360cf1883a616ab922028345df0b4891b76c81f4
FirmanJabar <firmanabduljabar@gmail.com>
2020-09-07 22:04:21 +0800

    :rocket: init

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,28 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
+  { href: 'https://github.com/firmanjabar', label: 'GitHub' },
   { href: 'https://nextjs.org/docs', label: 'Docs' },
-]
+];
 
 export default function Nav() {
   return (
     <nav>
-      <ul className="flex justify-between items-center p-8">
+      <ul className='flex justify-between items-center py-6 px-1 sm:px-2 lg:xy-8 xl:px-40 bg-gray-900'>
         <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
+          <Link href='/'>
+            <a className='text-teal-500 font-bold no-underline'>IVVIIXXII - 👨‍💻</a>
           </Link>
         </li>
-        <ul className="flex justify-between items-center space-x-4">
+        <ul className='flex justify-between items-center space-x-4'>
+          <li>
+            <Link href='/projects'>
+              <a className='text-teal-500 font-bold no-underline'>Projects</a>
+            </Link>
+          </li>
           {links.map(({ href, label }) => (
             <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
+              <a href={href} className='btn-blue no-underline' rel='norefferer' target='_blank'>
                 {label}
               </a>
             </li>
@@ -25,5 +30,5 @@ export default function Nav() {
         </ul>
       </ul>
     </nav>
-  )
+  );
 }
---
+++ pages/projects/index.js
@@ -0,0 +1,11 @@
+import Nav from '../../components/nav';
+import ProjectsComp from '../../components/projects';
+
+export default function Projects() {
+  return (
+    <>
+      <Nav />
+      <ProjectsComp />
+    </>
+  );
+}
