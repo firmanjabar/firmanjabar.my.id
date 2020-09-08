1627917b881b4aaa565cfcbbfb8fb5550e705d14
FirmanJabar <firmanabduljabar@gmail.com>
2020-09-08 10:58:55 +0800

    dev

--- components/icon/fontawesome.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCode, faHome, faFont } from '@fortawesome/free-solid-svg-icons';
-import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
-
-library.add(faCode, faHome, faGithubAlt, faFont);
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/index.css';
-import '../components/icon/fontawesome';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
