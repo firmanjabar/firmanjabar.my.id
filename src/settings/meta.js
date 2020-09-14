4304f42cfa11f43872ec719137e1d0a8b5f74689
FirmanJabar <firmanabduljabar@gmail.com>
2020-09-14 20:09:28 +0800

    add tag

--- src/settings/meta.js
+++ src/settings/meta.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: "Firman Jabar",
-  description: "FE Dev",
-  keywords: "firman, abdul, jabar",
-  url: "https://firmanjabar.my.id",
-  twitter: "@firmanjabar",
+  title: 'Firman Jabar',
+  description: 'FE Dev',
+  keywords: 'firman, abdul, jabar, firmanjabar, firmanabduljabar',
+  url: 'https://firmanjabar.my.id',
+  twitter: '@firmanjabar',
   // seoImage: "https://soulextract.com/images/soulextract.jpg",
-  color: "#000000",
+  color: '#000000',
 };
